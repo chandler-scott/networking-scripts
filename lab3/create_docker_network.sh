bfb7cc33cc8f9f43f11c18cef9202686431e4085
Chandler Scott <chandler.scott.dev@gmail.com>
2025-02-06 12:44:57 -0500

    fixed issues

--- lab3/create_docker_network.sh
+++ lab3/create_docker_network.sh
@@ -4,7 +4,18 @@ SUBNET="192.168.100.0/24"
 GATEWAY="192.168.100.1"
 INTERFACE="enp42s0"
 
+# Clean up
+docker compose down
+docker network rm macvlan_net
+
+# Create macvlan network
 docker network create -d macvlan \
 --subnet=$SUBNET \
 --gateway=$GATEWAY \
 -o parent=$INTERFACE macvlan_net
+
+# Stupid VirtualBox network requires this for VM2 to connect to VM1 containers
+sudo ip link set $INTERFACE promisc on
+
+# Restart for changes to take effect
+sudo systemctl restart networking
